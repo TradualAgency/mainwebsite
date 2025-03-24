bfe4be6d26193975f14b6a306906da1da86733fc
jordytje12 <jordy.vzanten2000@gmail.com>
2025-03-24 21:20:45 +0100

    new

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
       <>
         <h1>dit is een test push! test1234</h1>
           <p>Kijken hoe snel dit upload</p>
+          <p>Kijken of het nu goed werkt</p>
       </>
   )
 }
