3ac05ff7af9bca5057a84312def9b6b06e0d4aaa
jordytje12 <jordy.vzanten2000@gmail.com>
2025-04-02 21:41:09 +0200

    new

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,7 +1,11 @@
 import { getProjects } from "@/database/projecten/projects";
 
-export default function ProjectPage({ params }: { params: { id: string } }) {
-    const project = getProjects.find(p => p.id === params.id);
+type Params = Promise<{ id: string }>;
+
+export default async function ProjectPage({ params }: { params: Params }) {
+    const { id } = await params;
+
+    const project = getProjects.find(p => p.id === id);
 
     if (!project) {
         return <div>Project niet gevonden.</div>;
