549d5746023593a97130b01792981c8b2bb96e27
Jordy van Zanten <info@tradual.com>
2025-05-08 21:59:31 +0200

    new

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,4 +1,11 @@
-export function CenterLayout({children, className}){
+import {ReactNode} from "react";
+
+interface CenterLayoutProps{
+    children: ReactNode;
+    className?: string;
+}
+
+export function CenterLayout({children, className}: CenterLayoutProps){
     return(
         <section className="info-section my-20 px-8">
             <div className={`max-w-7xl mx-auto ${className} || ''`}>
