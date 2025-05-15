e15931f29fc7c1c3b54de5230927afcf7029eaf1
Jordy van Zanten <info@tradual.com>
2025-05-15 22:34:02 +0200

    new

--- containers/projecten/LottieSection.tsx
+++ containers/projecten/LottieSection.tsx
@@ -1,6 +1,12 @@
 'use client'
 
-import LottieScroll from '@/components/animejs-gsap/lottie/LottieScroll'
+import dynamic from 'next/dynamic'
+
+// Dynamically import LottieScroll with SSR disabled
+const LottieScroll = dynamic(
+    () => import('@/components/animejs-gsap/lottie/LottieScroll'),
+    { ssr: false }  // This ensures it only loads on the client side
+)
 
 export default function LottieSection() {
     return (
