771e00d55f280cd5371a08570cecfd4ab149fa8d
Jordy van Zanten <info@tradual.com>
2025-08-11 22:15:26 +0200

    new

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <USPSection />
         <TradualSection />
         <GrowthSection />
-          <CaseStudies />
+        <CaseStudies />
         <Kennisbank />
       </>
   )
